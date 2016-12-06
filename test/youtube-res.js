e9ad0f2b76f453e19e1800c6b646fc0941aa160d
klarkc <walker@praiseweb.com.br>
2016-12-06 11:56:59 -0300

    feat(youtube): add youtube response fixture

---
+++ test/youtube-res.js
@@ -0,0 +1,342 @@
+document.youtubeRes = {
+ "kind": "youtube#searchListResponse",
+ "etag": "\"5C5HHOaBSHC5ZXfkrT4ZlRCi01A/AYv7UziElVCONDzZK7kWwM0c_Kw\"",
+ "nextPageToken": "CAoQAA",
+ "regionCode": "BR",
+ "pageInfo": {
+  "totalResults": 3590,
+  "resultsPerPage": 10
+ },
+ "items": [
+  {
+   "kind": "youtube#searchResult",
+   "etag": "\"5C5HHOaBSHC5ZXfkrT4ZlRCi01A/7NWA6Wyucw9XDSxQJ-02zxhQOxA\"",
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "Jiq7EbzUMrw"
+   },
+   "snippet": {
+    "publishedAt": "2016-08-29T20:42:41.000Z",
+    "channelId": "UCakgsb0w7QB0VHdnCc-OVEA",
+    "title": "Video Replay: Stunning \"loops of fire\" on Sun - Incredible Time Lapse Videos Showing The Sun",
+    "description": "Featuring stunning pre-recorded video we taken a close up look of the suns surface , witnessing its power and awesome force. See the Full Video Here: ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/Jiq7EbzUMrw/default_live.jpg",
+      "width": 120,
+      "height": 90
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/Jiq7EbzUMrw/mqdefault_live.jpg",
+      "width": 320,
+      "height": 180
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/Jiq7EbzUMrw/hqdefault_live.jpg",
+      "width": 480,
+      "height": 360
+     }
+    },
+    "channelTitle": "Space Videos",
+    "liveBroadcastContent": "live"
+   }
+  },
+  {
+   "kind": "youtube#searchResult",
+   "etag": "\"5C5HHOaBSHC5ZXfkrT4ZlRCi01A/DpeBLNKcpLRo6n3CzEZpqqFYBb0\"",
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "qzMQza8xZCc"
+   },
+   "snippet": {
+    "publishedAt": "2016-10-22T13:30:44.000Z",
+    "channelId": "UCakgsb0w7QB0VHdnCc-OVEA",
+    "title": "NASA live stream - Earth From Space LIVE Feed | Incredible ISS live stream of Earth from space",
+    "description": "Live video of Earth from space - as seen from the Nasa ISS live stream aboard the International Space Station. The International Space Station - ISS - circles the ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/qzMQza8xZCc/default_live.jpg",
+      "width": 120,
+      "height": 90
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/qzMQza8xZCc/mqdefault_live.jpg",
+      "width": 320,
+      "height": 180
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/qzMQza8xZCc/hqdefault_live.jpg",
+      "width": 480,
+      "height": 360
+     }
+    },
+    "channelTitle": "Space Videos",
+    "liveBroadcastContent": "live"
+   }
+  },
+  {
+   "kind": "youtube#searchResult",
+   "etag": "\"5C5HHOaBSHC5ZXfkrT4ZlRCi01A/sTByflqDoC-iTIcoOVputEQLnGk\"",
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "YPv9yKC76hE"
+   },
+   "snippet": {
+    "publishedAt": "2016-09-23T07:53:02.000Z",
+    "channelId": "UC83RKJs4eKHVE9v0YUyqzgg",
+    "title": "Kitten Academy Live Stream",
+    "description": "If there is nothing going on, you can rewind the stream up to 4 hours to see action. Is there something we should know about? Do you have a question for us?",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/YPv9yKC76hE/default_live.jpg",
+      "width": 120,
+      "height": 90
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/YPv9yKC76hE/mqdefault_live.jpg",
+      "width": 320,
+      "height": 180
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/YPv9yKC76hE/hqdefault_live.jpg",
+      "width": 480,
+      "height": 360
+     }
+    },
+    "channelTitle": "Kitten Academy",
+    "liveBroadcastContent": "live"
+   }
+  },
+  {
+   "kind": "youtube#searchResult",
+   "etag": "\"5C5HHOaBSHC5ZXfkrT4ZlRCi01A/jS12KApBfeaNX1mMQNq9KAlmH8o\"",
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "SF7FUU7CThs"
+   },
+   "snippet": {
+    "publishedAt": "2016-11-24T13:08:30.000Z",
+    "channelId": "UCIR_LPmEQ9QHR0yB2lxgaxQ",
+    "title": "24/7 STREAM: 👽🌎 \"EARTH FROM SPACE\" ♥ NASA #SpaceTalk (2016) 1080p live HDVR | Subscribe now!",
+    "description": "24/7 Stream: Earth from Space ♥ NASA #SpaceTalk Live Chat - Rotate ISS HD Camera - #NoCGI #NoGreenScreen is presented. Earth from the ISS HDEV ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/SF7FUU7CThs/default_live.jpg",
+      "width": 120,
+      "height": 90
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/SF7FUU7CThs/mqdefault_live.jpg",
+      "width": 320,
+      "height": 180
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/SF7FUU7CThs/hqdefault_live.jpg",
+      "width": 480,
+      "height": 360
+     }
+    },
+    "channelTitle": "SPACE & UNIVERSE (Official)",
+    "liveBroadcastContent": "live"
+   }
+  },
+  {
+   "kind": "youtube#searchResult",
+   "etag": "\"5C5HHOaBSHC5ZXfkrT4ZlRCi01A/cl_HjPrk-MNUuSWqMuq3myFSI_0\"",
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "a-gJoO9A6so"
+   },
+   "snippet": {
+    "publishedAt": "2016-10-03T17:17:56.000Z",
+    "channelId": "UCSyg9cb3Iq-NtlbxqNB9wGw",
+    "title": "Shark Cam powered by EXPLORE.org",
+    "description": "Explore Multicam views, comment and post snapshots at http://explore.org Love Oceans - Subscribe http://goo.gl/hv2WXa http://explore.org - Facebook ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/a-gJoO9A6so/default_live.jpg",
+      "width": 120,
+      "height": 90
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/a-gJoO9A6so/mqdefault_live.jpg",
+      "width": 320,
+      "height": 180
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/a-gJoO9A6so/hqdefault_live.jpg",
+      "width": 480,
+      "height": 360
+     }
+    },
+    "channelTitle": "Explore Oceans",
+    "liveBroadcastContent": "live"
+   }
+  },
+  {
+   "kind": "youtube#searchResult",
+   "etag": "\"5C5HHOaBSHC5ZXfkrT4ZlRCi01A/gg7GrFyxlNG7t9O31o9NQ6Q426s\"",
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "wJUwiWwKUNc"
+   },
+   "snippet": {
+    "publishedAt": "2016-09-30T21:52:15.000Z",
+    "channelId": "UCSyg9cb3Iq-NtlbxqNB9wGw",
+    "title": "Cracroft Point - Surface powered by EXPLORE.org",
+    "description": "NEVER MISS AN ORCA ENCOUNTER! Sign up here for SMS alerts when Orca's arrive on camera: https://goo.gl/U5KQBt Explore Multicam views, comment and ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/wJUwiWwKUNc/default_live.jpg",
+      "width": 120,
+      "height": 90
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/wJUwiWwKUNc/mqdefault_live.jpg",
+      "width": 320,
+      "height": 180
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/wJUwiWwKUNc/hqdefault_live.jpg",
+      "width": 480,
+      "height": 360
+     }
+    },
+    "channelTitle": "Explore Oceans",
+    "liveBroadcastContent": "live"
+   }
+  },
+  {
+   "kind": "youtube#searchResult",
+   "etag": "\"5C5HHOaBSHC5ZXfkrT4ZlRCi01A/d9025a7EzJbkl-0K_v8ihTBUkSU\"",
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "gymPFiPE88I"
+   },
+   "snippet": {
+    "publishedAt": "2016-11-20T14:30:25.000Z",
+    "channelId": "UC0aJpP4DgTL96qnQE5BA1Sw",
+    "title": "Southwest Florida Eagle Cam",
+    "description": "",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/gymPFiPE88I/default_live.jpg",
+      "width": 120,
+      "height": 90
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/gymPFiPE88I/mqdefault_live.jpg",
+      "width": 320,
+      "height": 180
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/gymPFiPE88I/hqdefault_live.jpg",
+      "width": 480,
+      "height": 360
+     }
+    },
+    "channelTitle": "Southwest Florida Eagle Cam",
+    "liveBroadcastContent": "live"
+   }
+  },
+  {
+   "kind": "youtube#searchResult",
+   "etag": "\"5C5HHOaBSHC5ZXfkrT4ZlRCi01A/Ukoi4lkwoJMC09C6Nvk65ilIN5A\"",
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "3Ruruhg6q1w"
+   },
+   "snippet": {
+    "publishedAt": "2016-11-25T13:03:07.000Z",
+    "channelId": "UCakgsb0w7QB0VHdnCc-OVEA",
+    "title": "Video Replay: EARTH FROM SPACE - Gopro Video ISS Cosmonauts Carry Out SpaceWalk (2013)",
+    "description": "Incredible Gopro video footage from 2013 showing Russian cosmonauts carrying out a spacewalk and repairs to the ISS (International Space Station). Working ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/3Ruruhg6q1w/default_live.jpg",
+      "width": 120,
+      "height": 90
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/3Ruruhg6q1w/mqdefault_live.jpg",
+      "width": 320,
+      "height": 180
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/3Ruruhg6q1w/hqdefault_live.jpg",
+      "width": 480,
+      "height": 360
+     }
+    },
+    "channelTitle": "Space Videos",
+    "liveBroadcastContent": "live"
+   }
+  },
+  {
+   "kind": "youtube#searchResult",
+   "etag": "\"5C5HHOaBSHC5ZXfkrT4ZlRCi01A/OEtfEXH81nvBG3Ol9xzh1hiSTWU\"",
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "JfXiBkL9U5s"
+   },
+   "snippet": {
+    "publishedAt": "2016-11-18T13:16:16.000Z",
+    "channelId": "UCZXZQxS3d6NpR-eH_gdDwYA",
+    "title": "Ontario FeederWatch Cam, Sponsored by Perky-Pet®",
+    "description": "Thanks to Perky-Pet for helping to make the Ontario FeederWatch Cam possible! The FeederWatch cam is located in a residential neighborhood in ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/JfXiBkL9U5s/default_live.jpg",
+      "width": 120,
+      "height": 90
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/JfXiBkL9U5s/mqdefault_live.jpg",
+      "width": 320,
+      "height": 180
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/JfXiBkL9U5s/hqdefault_live.jpg",
+      "width": 480,
+      "height": 360
+     }
+    },
+    "channelTitle": "Cornell Lab Bird Cams Project",
+    "liveBroadcastContent": "live"
+   }
+  },
+  {
+   "kind": "youtube#searchResult",
+   "etag": "\"5C5HHOaBSHC5ZXfkrT4ZlRCi01A/UTwwdw8XSqwdteZnIv_f0fraj08\"",
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "UdmHHpAsMVw"
+   },
+   "snippet": {
+    "publishedAt": "2016-06-10T18:16:54.000Z",
+    "channelId": "UCLA_DiR1FfKNvjuUpBHmylQ",
+    "title": "NASA TV Public-Education",
+    "description": "NASA TV airs a variety of regularly scheduled, pre-recorded educational and public relations programming 24 hours a day on its various channels. Programs ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/UdmHHpAsMVw/default_live.jpg",
+      "width": 120,
+      "height": 90
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/UdmHHpAsMVw/mqdefault_live.jpg",
+      "width": 320,
+      "height": 180
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/UdmHHpAsMVw/hqdefault_live.jpg",
+      "width": 480,
+      "height": 360
+     }
+    },
+    "channelTitle": "NASA",
+    "liveBroadcastContent": "live"
+   }
+  }
+ ]
+};
